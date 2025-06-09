9ace402de9400c5168412d115974eccf5f8a1ccb
Student <Student@DESKTOP-6NP6TQ3>
2025-06-09 20:33:47 +0300

    база

--- 09.06.25/09.06.25/09.06.25.cpp
+++ 09.06.25/09.06.25/09.06.25.cpp
@@ -1,21 +1,34 @@
 ﻿#include <iostream>
+#include<string>
+#include<vector>
+
+
 using namespace std;
 
+vector<string> tasks;
+
+
+
+void addtask(const string& task) {
+    tasks.push_back(task);
+}
+
+
+void showTasks() {
+    cout << "СПИСОК: \n";
+        for (size_t i = 0; i < tasks.size(); i++) {
+            cout << i + 1 << "." << tasks[i] << endl;
+        }
+}
 
 
 int main()
 {
     setlocale(LC_ALL, "RU");
-   
-    //сходи за хлебом
-
-    // не хочу
-    
-    // надо
-
-    //ладно
-
-
+    addtask("сходить в школу");
+    addtask("погулять с собакой");
+    addtask("приготовить ужин");
+    showTasks();
 
     return 0;
 }
