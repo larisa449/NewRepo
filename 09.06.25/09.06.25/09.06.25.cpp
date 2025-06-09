2bbcbf11babcc54c73d2f638db97ce1d4d86ff41
Student <Student@DESKTOP-6NP6TQ3>
2025-06-09 19:39:49 +0300

    ВТОРОЙ

--- 09.06.25/09.06.25/09.06.25.cpp
+++ 09.06.25/09.06.25/09.06.25.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main()
 {
-    
+    setlocale(LC_ALL, "RU");
+    int name;
+    cout << "сколько тебе лет? \n";
+    cin >> name;
+    cout << "тебе: " << name << "лет";
 
 
 
